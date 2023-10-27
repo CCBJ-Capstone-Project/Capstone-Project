7b65e449ecd73ce5ef0e10881f2dd3dcaf5c29a4
Colin Meyer <crmeyer@crimson.ua.edu>
2023-10-26 19:26:23 -0500

    adding delete function to remove reviews from api

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -7,3 +7,26 @@ export const fetchReviews = async () => {
   // console.log(result);
   return result;
 }
+
+export const deleteReview = async (id) => {
+  try {
+    const response = await fetch(`${BASE_URL}/reviews/${id}`, 
+    {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
+    
+    if (response.status === 204) {
+      // Status code 204 indicates a successful deletion with no content.
+      return { success: true };
+    } else {
+      const result = await response.json();
+      return result;
+    }
+  } catch (error) {
+    console.error("An error occurred while deleting the review:", error);
+    return { success: false };
+  }
+}
--- client/src/components/SingleReviewPage.jsx
+++ client/src/components/SingleReviewPage.jsx
@@ -1,5 +1,6 @@
 import { useState,useEffect } from "react"
 import { useNavigate, useParams } from 'react-router-dom'
+import { deleteReview } from "../api";
 
 export default function SingleReview({ reviews }){
   const nav = useNavigate();
@@ -7,6 +8,20 @@ export default function SingleReview({ reviews }){
   const selectedReview = reviews.find((review) => {
     return review._id == reviewId;
   });
+
+  async function removeReview(review){
+    try{
+      const result = await deleteReview(review._id);
+      if(result.success){
+        console.log('Review deleted successfully!');
+        nav('/reviews');
+      } else{
+        console.error('Failed to delete review...');
+      }
+    } catch(error){
+      console.error(error);
+    }
+  }
 
   console.log(selectedReview);
 
@@ -16,6 +31,8 @@ export default function SingleReview({ reviews }){
   return(
     <>
       <h1>{selectedReview.title}</h1>
+      <h1>{selectedReview.message}</h1>
+      <button onClick={removeReview}>Delete</button>
     </>
   )
 }
