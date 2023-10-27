b6c788fa15a3e526a58b36c94cabc23e54de1b9a
JacobTThompson <jtt232@gmail.com>
2023-10-26 20:25:55 -0400

    Updated all users page

--- client/src/components/AllUsersPage.jsx
+++ client/src/components/AllUsersPage.jsx
@@ -1,9 +1,30 @@
 import { useState, useEffect } from "react";
+import {useNavigate} from 'react-router-dom'
 
-export default function AllUsersPage(){
+export default function AllUsersPage({users}){
+    const nav = useNavigate();
+
     return (
         <>
         <h1>All users Page</h1>
+        <div>
+        {
+            users.map((user) =>{
+                return(
+                    <>
+                    <div key={user.id} classname="user-container">
+                        <h2>{user.name}</h2>
+
+                        <button key= {user.id}
+                        onClick={() => nav(`/users/${user.id}`)}>
+                         See User Details
+                        </button>
+                    </div>
+                    </>
+                )
+            })
+        }
+        </div>
         </>
     )
 }
