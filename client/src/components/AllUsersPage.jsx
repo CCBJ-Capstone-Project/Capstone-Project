4aec554d0e8830d3bc8e374e0a62148ca42d53e3
behshadg <ghassemiben@gmail.com>
2023-10-31 20:44:08 -0400

    Test commit
    seeing if the template search bar is visible inside the navbar

--- client/src/components/AllUsersPage.jsx
+++ client/src/components/AllUsersPage.jsx
@@ -1,41 +1,38 @@
-import { useState, useEffect } from "react";
-import { useNavigate } from 'react-router-dom'
-import { showAllUsers } from "../api/usersUtils";
+import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { showAllUsers } from '../api/usersUtils';
 
-export default function AllUsersPage({users}){
+export default function AllUsersPage({ users }) {
   const nav = useNavigate();
   const [allUsers, setAllUsers] = useState([]);
 
-  async function displayUsers(){
+  async function displayUsers() {
     const usersDisplay = await showAllUsers();
     setAllUsers(usersDisplay);
   }
 
-useEffect(() => {
-  displayUsers();
-}, [])
+  useEffect(() => {
+    displayUsers();
+  }, []);
 
   return (
     <>
       <h1>All Users Page</h1>
       <div>
-        {
-          users.map((user) => {
-            return(
-              <>
-                <div key={user._id} className="user-container">
-                  <h2>{user.username}</h2>
+        {users.map((user) => {
+          return (
+            <>
+              <div key={user._id} className="user-container">
+                <h2>{user.username}</h2>
 
-                  <button key= {user.id}
-                  onClick={() => nav(`/users/${user._id}`)}>
-                    See User Details
-                  </button>
-                </div>
-              </>
-            )
-          })
-        }
+                <button key={user.id} onClick={() => nav(`/users/${user._id}`)}>
+                  See User Details
+                </button>
+              </div>
+            </>
+          );
+        })}
       </div>
     </>
-  )
+  );
 }
--- client/src/components/LoginPage.jsx
+++ client/src/components/LoginPage.jsx
@@ -1,4 +1,4 @@
-import { useState,useEffect } from "react"
+import { useState, useEffect } from 'react';
 
 const LoginPage = () => {
   const [email, setEmail] = usestate('');
@@ -7,19 +7,29 @@ const LoginPage = () => {
   const handleSubmit = () => {
     e.preventDefault();
     console.log(email);
-  }
+  };
 
-  return(
+  return (
     <>
-    <form>
-      <label for="email">email</label>
-      <input type = "email" placeholder="youremail@gmail.com" id="email" name="email"/>
-      <label for="password">password</label>
-      <input type="password" placeholder="youremail@gmail.com" id="password" name="password"/>
-    </form>
-    <button>Don't have an account? Register here.</button>
+      <form>
+        <label for="email">email</label>
+        <input
+          type="email"
+          placeholder="youremail@gmail.com"
+          id="email"
+          name="email"
+        />
+        <label for="password">password</label>
+        <input
+          type="password"
+          placeholder="youremail@gmail.com"
+          id="password"
+          name="password"
+        />
+      </form>
+      <button>Don't have an account? Register here.</button>
     </>
-  )
-}
+  );
+};
 
 export default LoginPage;
