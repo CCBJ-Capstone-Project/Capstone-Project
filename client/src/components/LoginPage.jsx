5398cc1ef96eb979784ba0e65b0304b1b4236cef
Colin Meyer <crmeyer@crimson.ua.edu>
2023-11-07 23:37:58 -0600

    registering creates new user in database / updates SingleUserPage rendering

--- client/src/components/LoginPage.jsx
+++ client/src/components/LoginPage.jsx
@@ -3,31 +3,31 @@ import {useNavigate} from "react-router-dom"
 
 export default function LoginPage(props) {
   const [email, setEmail] = useState('');
-  const [pass, setPass] = useState('');
+  const [password, setPassword] = useState('');
   const nav = useNavigate();
-   
- const handleSubmit = (e) => {
- e.preventDefault();
- console.log(email, pass);
- }
 
- const goToRegister=()=> {
-  nav("/register")
- }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email, password);
+  }
+
+  const goToRegister = () => {
+    nav("/register")
+  }
 
   return(
     <div className="auth-form-container">
       <h2>Login</h2>
-    <form onSubmit= {handleSubmit}>
-    <label>email
-     <input type = "email" value = {email} onChange={(e) => setEmail(e.target.value)}/>
-     </label>
-     <label >password
-     <input type = "password" value={pass} onChange={(e) => setPass(e.target.value)}/>
-     </label>
-     <button type ="submit">Log In</button>
-    </form>
-     <button onClick={goToRegister}>Don't have an account? Register here.</button> 
+      <form onSubmit= {handleSubmit}>
+        <label>Email: 
+          <input type = "email" value = {email} onChange={(e) => setEmail(e.target.value)}/>
+        </label>
+        <label>Password: 
+          <input type = "password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+        </label>
+        <button type ="submit">Log In</button>
+      </form>
+      <button onClick={goToRegister}>Don't have an account? Register here.</button> 
     </div>
   )
 }
