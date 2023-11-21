caf9c02238e70ac75148e4ca8de8e5fbb5dd9375
Colin Meyer <crmeyer@crimson.ua.edu>
2023-11-20 21:40:34 -0600

    set up database on web server

--- server/index.js
+++ server/index.js
@@ -10,8 +10,19 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
+// Serve static files from the 'dist' folder (client build output)
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
 const CONNECTION_URL = 'mongodb+srv://ccbj_capstone:ccbj_capstone123@capstonedatabase.j8gktwu.mongodb.net/';
 const PORT = process.env.PORT || 8080;
+
+app.get('/', (req, res, next) => {
+  try {
+    res.send('APP IS RUNNING!')
+  } catch (error) {
+    next(error);
+  }
+})
 
 mongoose.connect(CONNECTION_URL)
   .then(() => app.listen(
@@ -20,17 +31,9 @@ mongoose.connect(CONNECTION_URL)
   ))
   .catch((error) => console.log(error));
 
-app.get('/', (req, res, next) => {
-  try {
-    res.send('Hello From Home Page')
-  } catch (error) {
-    next(error);
-  }
-})
-
-
 import usersRoutes from './routes/users.js';
 app.use('/users', usersRoutes);
 
 import reviewsRoutes from './routes/reviews.js';
 app.use('/reviews', reviewsRoutes);
+
